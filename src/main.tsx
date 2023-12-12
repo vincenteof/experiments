576664325586ff5bc481d8fefc15648fb8121ed9
vincenteof <14847208+vincenteof@users.noreply.github.com>
2023-12-12 23:02:27 +0800

    chore: react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './routes/root'
+import Index from './routes/index'
+import ErrorPage from './error-page.tsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [{ index: true, element: <Index /> }],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,7 @@
-function App() {
+export default function Index() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   )
 }
-
-export default App
